76f3d5a3d801da37a482bfa657356a3759e1aa1f
cph101 <cph101@icloud.com>
2024-02-21 11:52:45 +0000

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/christianhiemstra/Desktop/Personal/Dev/Lets-Do-Wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/christianhiemstra/Desktop/Personal/Dev/Lets-Do-Wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/christianhiemstra/Desktop/Personal/Dev/Lets-Do-Wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/christianhiemstra/Desktop/Personal/Dev/Lets-Do-Wiki/src/css/custom.css"),
-];
