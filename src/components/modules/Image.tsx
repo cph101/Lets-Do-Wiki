826001443e3d4c41bbe83ebc67f78646e1779598
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-06 15:49:06 +0200

    Fix: Image

--- src/components/modules/Image.tsx
+++ src/components/modules/Image.tsx
@@ -1,4 +1,4 @@
-export function Image({ modId, imageId, align }) {
+export default function Image({ modId, imageId, align }) {
   return (
     <div align={align}>
       <img src={`/Lets-Do-Wiki/img/textures/${modId}/image/${imageId}`} alt="display" width="500"/>
