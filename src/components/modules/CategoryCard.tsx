da15e11003a2a068822536fae0ab316a5619bab1
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-06 20:48:54 +0200

    Update CategoryCard.tsx

--- src/components/modules/CategoryCard.tsx
+++ src/components/modules/CategoryCard.tsx
@@ -6,8 +6,7 @@ const CategoryCard = ({ category }) => {
     <div className="padding-vert--lg padding-horiz--lg">
       <Link to={`/docs/${category.slug}`} className="card">
         <div className="card__body">
-          {/* Replace text with logo image */}
-          <img src={`/Lets-Do-Wiki/img/logo/${category.logo}`} alt={category.name} className="card__image" />
+          <img src={`/Lets-Do-Wiki/img/logo/${category.logo}`} alt={category.name} className="card__image" style={{ width: '50%', height: 'auto' }} />
           <div className="card__title">{category.name}</div>
         </div>
       </Link>
