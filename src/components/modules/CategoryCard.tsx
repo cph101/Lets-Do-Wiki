4cc51c9ce5be6cab6a9729160b5cbfaea5821314
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-06 21:15:53 +0200

    Fix: Category Cards

--- src/components/modules/CategoryCard.tsx
+++ src/components/modules/CategoryCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
+import './CategoryCard.css'; // Import the CSS file with your custom styles
 
 const CategoryCard = ({ category }) => {
   return (
-    <div className="padding-vert--lg padding-horiz--lg">
+    <div className="cardContainer_fWXF">
       <Link to={`/docs/${category.slug}`} className="card">
         <div className="card__body">
-          <img src={`/Lets-Do-Wiki/img/logo/${category.logo}`} alt={category.name} className="card__image" style={{ width: '50%', height: 'auto' }} />
+          <img src={`/Lets-Do-Wiki/img/logo/${category.logo}`} alt={category.name} className="card__image" />
           <div className="card__title">{category.name}</div>
         </div>
       </Link>
