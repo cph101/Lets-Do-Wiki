3ed767ca02010f694e880e7098b0fd99843d9d17
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-10 19:55:56 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,13 +56,13 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     algolia: {
-          // The application ID provided by Algolia
           appId: 'GQS7PRJCVR',
-
-          // Public API key: it is safe to commit it
           apiKey: '096a1ff6d5aa10a0b108b3478062a0ae',
           indexName: 'team-let-s-doio',
           contextualSearch: false,
+          searchParameters: {
+                            facetFilters: ['language:en']
+                          }
         },
     colorMode: {
           defaultMode: 'dark',
