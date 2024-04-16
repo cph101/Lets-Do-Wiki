b7460d95cad6bafc5627b2c1957d3472f13d41ef
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-16 11:21:18 +0200

    please

--- src/components/modules/EffectList.tsx
+++ src/components/modules/EffectList.tsx
@@ -16,14 +16,13 @@ export default function EffectList({ modId, list }) {
   return (
     <div>
       {list.map((item, index) => (
-        <span key={index} style={{ display: 'grid', gridTemplateColumns: 'auto 1fr', alignItems: 'center' }}>
+        <div key={index} style={{ display: 'grid', gridTemplateColumns: 'auto 1fr', alignItems: 'center' }}>
           <Image modId={modId} imageUrl={item.imageId} />
-          <span className="speckyInlineMarkdown" style={{ marginLeft: '8px' }}>
-            <ReactMarkdown>
-              <strong>{item.name}</strong> - {item.description}
-            </ReactMarkdown>
-          </span>
-        </span>
+          <div className="speckyInlineMarkdown" style={{ marginLeft: '8px' }}>
+            <strong>{item.name}</strong>
+            <ReactMarkdown>{` - ${item.description}`}</ReactMarkdown>
+          </div>
+        </div>
       ))}
     </div>
   );
