332a2b77e7602111be7632a4ed920ae5f97f894b
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2024-04-16 10:36:21 +0200

    update: effectlist

--- src/components/modules/EffectList.tsx
+++ src/components/modules/EffectList.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
-export function Image({ modId, type, imageUrl }) {
+export function Image({ modId, imageUrl }) {
   return (
     <span>
       <img
         src={`/Lets-Do-Wiki/img/textures/${modId}/${type}/${imageUrl}`}
-        style={{ imageRendering: 'pixelated', width: '50px', marginRight: '4px' }}
+        style={{ imageRendering: 'pixelated', width: '50px', marginRight: '4px', marginBottom: "-25px"}}
       />
     </span>
   );
 }
 
-export default function EffectList({ modId, list, type }) {
+export default function EffectList({ modId, list}) {
     return (
         <div>
             {list.map((item, index) => (
                 <span key={index} style={{marginBottom: '4px'}}>
-                    <Image modId={modId} type={type} imageUrl={item.imageId}/>
+                    <Image modId={modId} imageUrl={item.imageId}/>
                     <span style={{fontWeight: 'bold'}}>{item.name}</span>
                     <span> - </span>
                     <span className="speckyInlineMarkdown"><ReactMarkdown>{item.description}</ReactMarkdown></span>
